27c7a5950130f614ea6ec85438f417dccba56dfd
Elias Leitinger <eliasleiti@gmail.com>
2021-04-19 21:28:53 +0200

    Add AGPL-3.0-or-later License

--- define.h
+++ define.h
@@ -1,4 +1,26 @@
 /*
+ *
+ *  Microsim (µsim) - small life simulation game
+ *  Copyright (C) 2021 Elias Leitinger (eliasleiti@gmail.com)
+ *
+ *  usim is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU Affero General Public License as
+ *  published by the Free Software Foundation, either version 3 of the
+ *  License, or any later version.
+ *
+ *  usim is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU Affero General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Affero General Public License
+ *  along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ *
+ */
+
+
+
+/*
  *Definitions used for Creating the User interface and other thigns 
  *customize it to fit your terminal size. Making them smaller _will_
  *break things.
